3a0bb07c3f11d82d0d0122f956c4291ee8c8de32
David Konerding <dakoner@gmail.com>
2014-07-07 20:33:44 -0700

    add SDL input.

---
+++ sdl_input.h
@@ -0,0 +1,34 @@
+#ifndef _SDL_INPUT_H_
+#define _SDL_INPUT_H_
+
+#include <iostream>
+#include "SDL2/SDL.h"
+#include "input.h"
+
+class SDLJoystick: public Joystick {
+  bool readButton(unsigned char channel) {
+    SDL_PumpEvents();
+    return !SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT);
+  }
+  int readJoystickX() {
+    SDL_PumpEvents();
+    int x;
+    SDL_GetMouseState(&x, NULL);
+    x= int((x-gfx->width()/2) / float(gfx->width()) * 25);
+    std::cout << "x: " << x << std::endl;
+    return x;
+  }
+
+  int readJoystickY() {
+    SDL_PumpEvents();
+    int y;
+    SDL_GetMouseState(NULL, &y);
+
+    
+    y =  int((y-gfx->width()/2) / float(gfx->height()) * 25);
+    std::cout << "y: " << y << std::endl;
+    return y;
+  }
+};
+
+#endif
