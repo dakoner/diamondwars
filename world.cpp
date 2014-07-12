0cb1a31f4e42c03c72f48536c49fe7da044919fe
David Konerding <dakoner@gmail.com>
2014-07-12 15:11:34 -0700

    add a bit more detail aand challenge to the world.

--- world.cpp
+++ world.cpp
@@ -1,11 +1,11 @@
 #include "world.h"
 #include "env.h"
 
-#define NUM_HEIGHTS 32
+#define NUM_HEIGHTS 38
 
 World::World(Vec2 position, Color color, Vec2 velocity): MovingObject(position, color, velocity) {
-  int st[NUM_HEIGHTS] = {10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 30, 10, 10, 60, 10, 20, 20, 30, 30, 40, 40, 50, 50, 40, 40, 30, 30, 20, 20, 10, 10};
-  int sm[NUM_HEIGHTS] = {10, 10, 30, 10, 10, 60, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 20, 20, 30, 30, 40, 40, 50, 50, 40, 40, 30, 30, 20, 20, 10, 10};
+  int st[NUM_HEIGHTS] = {10, 10, 10, 10, 10, 30, 10, 10, 10, 10, 10, 30, 10, 10, 60, 10, 20, 20, 30, 30, 40, 40, 50, 50, 40, 40, 30, 30, 20, 20, 10, 10, 10, 70, 70, 10, 10};
+  int sm[NUM_HEIGHTS] = {10, 10, 30, 10, 10, 60, 10, 10, 10, 30, 10, 10, 10, 10, 10, 10, 20, 20, 30, 30, 40, 40, 50, 50, 40, 40, 30, 30, 20, 20, 10, 10, 10, 10, 10, 10, 70};
     _stalagtites.assign(st, st+NUM_HEIGHTS);
     _stalagmites.assign(sm, sm+NUM_HEIGHTS);
   }
